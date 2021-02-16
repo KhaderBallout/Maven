d249272f6bf90b74291c6ad8ed79afdacf0bf048
Khader Ballout <khaderj@hotmail.com>
2021-02-16 11:18:23 +0200

    Assignment1

---
+++ src/main/java/edu/bethlehem/Assignment1/Assignment1Main.java
@@ -0,0 +1,23 @@
+package edu.bethlehem.Assignment1;
+
+/**
+ * This class will show the output for assignment 1
+ * 
+ * @author Khader Ballout//TODO
+ *
+ */
+
+public class Assignment1Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentName = "Khader Ballout";
+		long studentNumber = 201801418;
+		System.out.printf("\n\n\n================================\n");
+		System.out.printf("==========Assignment 1==========\n");
+		System.out.printf("This is prepared by %s\n", studentName);
+		System.out.printf("                    %s\n", studentNumber);
+		System.out.printf("================================\n\n\n");
+	}
+
+}
